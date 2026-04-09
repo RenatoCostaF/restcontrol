4fe41b9e426d8f5afc908e46c89fe3f5bbacd875
Renato <renatofurtado33@gmail.com>
2026-04-09 00:35:53 -0300

    first commit

---
+++ src/main/java/com/restocontrol/restcontrol_api/RestcontrolApiApplication.java
@@ -0,0 +1,13 @@
+package com.restocontrol.restcontrol_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcontrolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcontrolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restocontrol/restcontrol_api/RestcontrolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restocontrol.restcontrol_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
