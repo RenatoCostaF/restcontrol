a58f386e99b9cca7d25425a739e86ea708dfd507
gabrielressa <gabriel.santos@snyk.io>
2026-04-10 20:42:20 -0300

    Initial Commit

---
+++ src/main/java/controllers/handlers/UserController.java
@@ -0,0 +1,41 @@
+package controllers.handlers;
+
+/*
+import org.apache.catalina.User;
+*/
+import entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping
+    public ResponseEntity<List<User>> findUserByName(
+            @PathVariable("name") String name
+    ){
+        logger.info("/user"+name);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(){
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(){
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> changePassword(){
+
+    }
+
+}
