7d0d33385f2010b03b33ec3f639c676353adc768
gabrielressa <gabriel.santos@snyk.io>
2026-04-13 18:09:23 -0300

    Update Controller class

--- src/main/java/controllers/handlers/UserController.java
+++ src/main/java/controllers/handlers/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import services.UserService;
 
 import java.util.List;
 
@@ -16,21 +17,36 @@ import java.util.List;
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{name}")
     public ResponseEntity<List<User>> findByName(
             @PathVariable("name") String name
     ){
         logger.info("/user/"+name);
+        var sysUser = this.userService.findByName(name);
         return ResponseEntity.ok(user);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateUser(){
+    public ResponseEntity<Void> updateUser(
+            @PathVariable("id") Long id,
+            @RequestBody User user
+    ){
+        logger.info("PUT => /user"+id);
+        this.userService.updateUser(user,id);
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(){
-
+    public ResponseEntity<Void> createUser(
+            @RequestBody User user
+    ){
+        logger.info("POST => /user");
+        this.userService.saveUser(user);
     }
 
     @PutMapping
@@ -38,8 +54,12 @@ public class UserController {
 
     }
 
-    public ResponseEntity<Void> deleteUser(){
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(
+            @PathVariable("id") Long id
+    ){
+        logger.info("DELETE => /user/"+id);
+        this.userService.deleteUser(id);
     }
 
 }
