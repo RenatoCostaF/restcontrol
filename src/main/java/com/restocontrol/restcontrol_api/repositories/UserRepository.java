9766ee14aee75c40a36ce77d4ea2d7e1392838df
Renato Furtado <renatofurtado33@gmail.com>
2026-04-15 21:53:47 -0300

    fix

---
+++ src/main/java/com/restocontrol/restcontrol_api/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.restocontrol.restcontrol_api.repositories;
+
+import com.restocontrol.restcontrol_api.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByName(String name);
+}
