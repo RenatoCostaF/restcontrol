26be53d1b7560a48b7e0c732d7b0a975b9b2f265
gabrielressa <gabriel.santos@snyk.io>
2026-04-20 10:47:23 -0300

    Update changePasswordDTO

--- src/main/java/com/restocontrol/restcontrol_api/DTOs/ChangePasswordDTO.java
+++ src/main/java/com/restocontrol/restcontrol_api/DTOs/ChangePasswordDTO.java
@@ -2,7 +2,7 @@ package com.restocontrol.restcontrol_api.DTOs;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record ChangePasswordDTO {
-    @NotBlank(message = "Password is required")
-    String password;
+public record ChangePasswordDTO (
+        @NotBlank(message = "Password is required")
+        String password){
 }
