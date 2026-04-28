d1654d589546a5648f75ba91086d37a02ea4d857
Renato Furtado <renatofurtado33@gmail.com>
2026-04-27 21:59:45 -0300

    fix

--- src/main/java/com/restocontrol/restcontrol_api/config/OpenApiConfig.java
+++ src/main/java/com/restocontrol/restcontrol_api/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.restocontrol.restcontrol_api.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +15,23 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI locaTech(){
+    public OpenAPI locaTech() {
         return new OpenAPI()
                 .info(
                         new Info().title("Gestao Usuarios - Restaurantes")
                                 .description("Projeto desenvolvido para o Tech-Challenge - Fase 1")
                                 .version("v0.0.1")
                                 .license(new License().name("restcontrol").url("https://github.com/RenatoCostaF/restcontrol"))
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT"))
+
                 );
     }
 }
